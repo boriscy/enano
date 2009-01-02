67ac61a859a28f7adef1a51ad8f69d90ff7e8fce
boris <boriscyber@gmail.com>
2009-01-02 18:51:05 -0400

    Modificaciones en TreeCreator para que no presente warnings (parentesis)

--- lib/tree_creator.rb
+++ lib/tree_creator.rb
@@ -1,4 +1,7 @@
-#Generates a Tree
+# Generates a Tree
+# @author Boris Barroso boriscyber@gmail.com
+# Recives a result from a query when using betternestedset plugin
+# and creates a Hash with all children
 class TreeCreator
 
   #default values
@@ -6,34 +9,36 @@ class TreeCreator
     @node = params.length > 0 ? params : {:id => :id, :text => :text, :iconCls => :icon_cls, :include => []}
   end
 
+  # Creates the Tree
+  # @param Array arr
   def create_tree(arr)
 
     tree = []
     arr.each do |v|
       if v.parent.nil?
-        tree.push( set_node v)
+        tree.push( set_node(v))
       else
-        parent_in_array tree, v.parent_id
+        parent_in_array(tree, v.parent_id)
         @search_node[:children].push(set_node v)
       end
     end
     tree
   end
-  
+
   protected
-  
-  #Creates a node for Ext Tree
+
+  # Creates a node for Ext Tree
   def set_node(v)
-    node = {:children => [], :singleClickExpand => true}
+    node = {:children => []}
     @node[:include].each do |model|
       modelPar = v.send(model[:model])
-      node.merge!( set_params modelPar, model[:attributes] )
+      node.merge!( set_params( modelPar, model[:attributes]) )
     end if @node[:include]
-    node.merge!(set_params  v)
+    node.merge!(set_params(v))
     node
   end
 
-  #Finds the parent node in array
+  # Finds the parent node in array
   def parent_in_array(arr, id)
     arr.each do |val|
       if val[:id] == id
@@ -45,6 +50,7 @@ class TreeCreator
     end
   end
 
+  # Set the params
   def set_params(vals, keys = @node)
     node = {}
     keys.each {|key, val| node[key] = vals.send(val) if key && key != :include}
